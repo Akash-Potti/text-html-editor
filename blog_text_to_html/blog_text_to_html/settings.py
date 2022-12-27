3a5e32cf9494333061883047c2be71291f0b329f
Akash-Potti <akashpotti06@gmail.com>
2022-12-27 18:22:33 +0530

    vercel-2

--- blog_text_to_html/blog_text_to_html/settings.py
+++ blog_text_to_html/blog_text_to_html/settings.py
@@ -59,16 +59,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'blog_text_to_html.wsgi.application'
 
-DATABASES = {}
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  #  'default': {
+  #     'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
 
 
@@ -107,6 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
